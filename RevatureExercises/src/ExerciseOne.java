55e1f5ba117d0ab95ee573663fd59d72f1d6d331
Aashish Thapa Magar <tsukyomi007@yahoo.com>
2021-10-01 23:50:01 -0500

    Week 1 Exercises

---
+++ RevatureExercises/src/ExerciseOne.java
@@ -0,0 +1,15 @@
+
+public class ExerciseOne {
+
+	public static void main(String[] args) {
+		//Declare two double variables to store the two values.
+		double var_1= 10.5, 
+			   var_2= 11.5;
+		
+		//Declare a double variable to hold the multiplication of the two values.
+        double result = var_1 * var_2;
+        
+        //Print the result.
+        System.out.println("result: " + result);
+	}
+}
---
+++ RevatureExercises/src/ExerciseTwo.java
@@ -0,0 +1,16 @@
+
+public class ExerciseTwo {
+
+	public static void main(String[] args) {
+		int x = 2;
+		
+		//If else statement: if true execute line 9, if false execute line 12.
+		if (x == 3) {
+			System.out.println("x is equal to 3");
+		}
+		else {
+			System.out.println("x is NOT equal to 3");
+		}
+	}
+
+}
